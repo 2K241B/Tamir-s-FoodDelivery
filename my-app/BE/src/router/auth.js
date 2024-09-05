9a1358800dc534e1fdb3925ff200e6773164bf03
24LP4205 24LP4205 <24LP4205@G301-CC0207.local>
2024-09-05 15:42:11 +0800

    createuser

---
+++ my-app/BE/src/router/auth.js
@@ -0,0 +1,8 @@
+import { Router} from "express";
+import { createUser, getUser } from "../controller/auth.js";
+
+const userRouter = Router();
+
+userRouter.post("/create", createUser).get("/:id", getUser);
+
+export { userRouter };
