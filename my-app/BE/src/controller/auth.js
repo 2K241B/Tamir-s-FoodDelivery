47f1ae8d76d84a63e81db9d84af41fa74a0474a1
24LP4205 24LP4205 <24LP4205@G301-CC0207.local>
2024-09-05 17:16:57 +0800

    zasav create user and schema

--- my-app/BE/src/controller/auth.js
+++ my-app/BE/src/controller/auth.js
@@ -4,26 +4,26 @@ import bcrypt from "bcrypt";
 const saltRounds = 10;
 
 export const createUser = async (req, res) => {
-  const { email, name, password, phoneNumber, role } = req.body;
-
-  try {
-    bcrypt.genSalt(saltRounds, (err, salt) => {
-      bcrypt.hash(password, salt, async (err, hash) => {
-        const response = await userModel.create({
-          email,
-          name,
-          password: hash,
-          phoneNumber,
-          role,
-        });
-        res.status(200).send(response);
+    const { email, name, password, phoneNumber, role } = req.body;
+  
+    try {
+      const salt = await bcrypt.genSalt(saltRounds);
+      const hash = await bcrypt.hash(password, salt);
+  
+      const response = await userModel.create({
+        email,
+        name,
+        password: hash,
+        phoneNumber,
+        role,
       });
-    });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send(error.message);
-  }
-};
+  
+      res.status(200).send(response);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send(error.message);
+    }
+  };
 
 
 export const getUser = async (req, res) => {
