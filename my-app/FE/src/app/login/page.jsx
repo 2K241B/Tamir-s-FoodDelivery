3501b9ef1a0d05d9968265aaca1b2a24302f0f53
Tamirlp4205 <tamir.b8550@gmail.com>
2024-09-10 13:05:52 +0800

    signup UI

--- my-app/FE/src/app/login/page.jsx
+++ my-app/FE/src/app/login/page.jsx
@@ -1,115 +1,116 @@
-"use client";
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { EyeIcon, EyeOff } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useMemo, useState } from 'react';
+import debounce from 'lodash/debounce';
+import { axiosInstance } from '@/lib/axios';
 
-import { useState } from "react";
-import { Button } from "@/components/ui/button";
-import { EyeIcon, EyeOff } from "lucide-react";
-import { Input } from "@/components/ui/input";
-import axios from 'axios';
-import Cookies from 'js-cookie';
-import { useRouter } from "next/navigation";
-import axiosInstance from "@/lib/axios";
-
-const buttonStyles = {
-    active: "rounded-[4px] bg-green-600 w-full text-slate-200",
-    notActive: "rounded-[4px] bg-[#EEEFF2] w-full text-slate-200"
+export const styles = {
+  container:
+    'w-[448px] h-fit rounded-[16px] flex flex-col gap-[48px]  p-8 bg-white m-auto pt-[143px] pb-[107px]',
+  header: 'text-[#0D1118] text-center text-[28px] font-bold',
+  form: 'flex flex-col items-start gap-4 w-full text-sm',
+  inputContainer: 'flex flex-col gap-1 w-full text-sm',
+  subContainer: 'flex flex-col w-full gap-8 items-center text-sm',
+  input:
+    'w-full flex items-center justify-between border-[#ECEDF0] border-[0.5px] bg-[#F7F7F8] text-[#8B8E95] rounded-[4px] pr-3',
+  ButtonStyle1:
+    'disabled:bg-[#EEEFF2] disabled:text-[#1C20243D] font-normal px-4 py-2 bg-[#18BA51] text-white',
+  ButtonStyle2:
+    'bg-white border-[#18BA51] border-[1px] text-[#272727] font-normal px-4 py-2 hover:bg-[#18BA51] hover:text-white',
+  borderOff: 'bg-[#F7F7F8] border-0',
 };
 
-const LoginPage = () => {
-    const [formData, setFormData] = useState({
-        email: "",
-        password: "",
-        passwordShown: false
-    });
-    const router = useRouter()
-    const [isLoading, setIsLoading] = useState(false);
-    const [error, setError] = useState("");
+export const page = () => {
+  const router = useRouter();
+  const [isHidePassword, setIsHidePassword] = useState(true);
+  const [formData, setFormData] = useState({ email: '', password: '' });
+  const togglePasswordVisibility = () => setIsHidePassword((prev) => !prev);
+  const Icon = isHidePassword ? EyeOff : EyeIcon;
 
-    const togglePasswordVisibility = () => {
-        setFormData(prev => ({ ...prev, passwordShown: !prev.passwordShown }));
-    };
+  const handleOnChange = (event) => {
+    setFormData((prev) => ({
+      ...prev,
+      [event.target.name]: event.target.value,
+    }));
+  };
 
-    const handleOnChange = (e) => {
-        const { name, value } = e.target;
-        setFormData(prev => ({ ...prev, [name]: value }));
-    };
+  const debounceFn = useMemo(() => debounce(handleOnChange, 500), []);
 
-    const handleLogin = async (e) => {
-        e.preventDefault();
-        setIsLoading(true);
-        setError("");
-        try {
-            const response = await axiosInstance.post('/auth/login', {
-                email: formData.email,
-                password: formData.password
-            });
-            if (response.status== 200) router.push("/")
-        } catch (error) {
-            console.error(error);
-            if (error.response && error.response.status === 401) {
-                setError("Incorrect email or password");
-            } else {
-                setError("Login failed. Please try again.");
-            }
-        } finally {
-            setIsLoading(false);
-        }
-    };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
 
-    return (
-        <div className="w-[448px] rounded-[16px] p-[32px] flex flex-col gap-[48px] m-auto">
-            <div>
-                <h1 className="font-bold text-center text-[28px]">Нэвтрэх</h1>
-            </div>
-            <form className="flex flex-col w-full gap-2" onSubmit={handleLogin}>
-                <div>
-                    <p className="font-normal text-[14px] ml-1">Имэйл</p>
-                    <Input
-                        className="h-12 border-none bg-[#F7F7F8]"
-                        placeholder="Имэйл хаягаа оруулна уу"
-                        type="email"
-                        name="email"
-                        onChange={handleOnChange}
-                        required
-                    />
-                </div>
-                <div className="relative">
-                    <p className="font-normal text-[14px] ml-1">Нууц үг</p>
-                    <Input
-                        className="h-12 border-none bg-[#F7F7F8]"
-                        placeholder="Нууц үг"
-                        type={formData.passwordShown ? "text" : "password"}
-                        name="password"
-                        onChange={handleOnChange}
-                        required
-                    />
-                    <div
-                        className="absolute cursor-pointer right-4 top-8"
-                        onClick={togglePasswordVisibility}
-                    >
-                        {formData.passwordShown ? <EyeIcon /> : <EyeOff />}
-                    </div>
-                    <p className="font-normal text-[14px] mt-2 flex justify-end">Нууц үг сэргээх</p>
-                </div>
-                {error && <p className="text-center text-red-500">{error}</p>}
-                <div className="w-full flex flex-col gap-[32px] items-center">
-                    <Button
-                        className={formData.email !== "" && formData.password !== "" ? 'rounded-[4px] bg-green-600 w-full text-slate-200 hover:bg-green-600' : 'rounded-[4px]  w-full bg-white text-slate-200 hover:bg-green-600 '}
-                        type="submit"
-                        disabled={isLoading}
-                    >
-                        {isLoading ? 'Ачааллаж байна...' : 'Нэвтрэх'}
-                    </Button>
-                    <p className="font-normal text-[14px]">Эсвэл</p>
-                    <Button
-                        variant="default"
-                        className="rounded-[4px] border-green-500 border-solid border-[1px] bg-white text-black w-full hover:bg-green-600"
-                    >
-                        Бүртгүүлэх
-                    </Button>
-                </div>
-            </form>
+    try {
+      const response = await axiosInstance.post('/auth/login', {
+        email: formData.email,
+        password: formData.password,
+      });
+      if (response.status === 200) router.push('/');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  return (
+    <form onSubmit={handleSubmit} className={styles.container}>
+      <div className={styles.header}>
+        <h2>Нэвтрэх</h2>
+      </div>
+      <div className={styles.form}>
+        <div className={styles.inputContainer}>
+          <h3>Имэйл </h3>
+          <Input
+            name="email"
+            type="email"
+            onChange={debounceFn}
+            placeholder="Имэйл хаягаа оруулна уу"
+            className={styles.input}
+            required
+          />
         </div>
-    );
+        <div className={styles.inputContainer}>
+          <h3>Нууц үг</h3>
+          <div className={styles.input}>
+            <Input
+              name="password"
+              type={isHidePassword ? 'password' : 'text'}
+              onChange={debounceFn}
+              placeholder="Нууц үг"
+              className={styles.borderOff}
+              required
+            ></Input>
+            <Icon
+              onClick={togglePasswordVisibility}
+              className="cursor-pointer"
+            />
+          </div>
+          <p className="text-end">Нууц үг сэргээх</p>
+        </div>
+      </div>
+      <div className={styles.subContainer}>
+        <Button
+          disabled={
+            formData.email.length > 0 && formData.password.length > 0
+              ? false
+              : true
+          }
+          type="submit"
+          className={styles.ButtonStyle1}
+        >
+          Нэвтрэх
+        </Button>
+        <p>Эсвэл</p>
+        <Button
+          type="button"
+          onClick={() => router.push('/signup')}
+          className={styles.ButtonStyle2}
+        >
+          Бүртгүүлэх
+        </Button>
+      </div>
+    </form>
+  );
 };
 
-export default LoginPage;
+export default page;
