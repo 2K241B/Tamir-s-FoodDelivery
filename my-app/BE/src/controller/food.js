441eb847f25a81274e66a41030eaab757859885c
Tamirlp4205 <168177795+Tamirlp4205@users.noreply.github.com>
2024-09-17 14:54:08 +0800

    update food (#115)
    Co-authored-by: 24LP4205 24LP4205 <24LP4205@G301-CC0207.local>

--- my-app/BE/src/controller/food.js
+++ my-app/BE/src/controller/food.js
@@ -41,3 +41,26 @@ export const getFoods = async (req, res) => {
       return res.status(500).json(error);
     }
   };
+
+
+
+  export const updateFood = async (req, res) => {
+    const { id } = req.params;
+  
+    const { name, image, ingeredient, price, discount, categoryId } = req.body;
+  
+    try {
+      const response = await foodModel.findByIdAndUpdate(id, {
+        name,
+        image,
+        ingeredient,
+        price,
+        discount,
+        categoryId,
+      });
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  };
