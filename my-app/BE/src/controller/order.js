31cdae889208cce3b4813d7dcb76d0ff68af98e5
Tamirlp4205 <168177795+Tamirlp4205@users.noreply.github.com>
2024-09-17 13:20:57 +0800

    102 be get all order (#104)
    * get all order
    
    * get one order

--- my-app/BE/src/controller/order.js
+++ my-app/BE/src/controller/order.js
@@ -35,3 +35,28 @@ export const createOrder = async (req, res) => {
     return res.status(500).json({ error: 'Failed to create order' });
   }
 };
+
+export const getAllOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find(); 
+    return res.status(200).json(orders);
+  } catch (error) {
+    console.error('Error fetching orders:', error);
+    return res.status(500).json({ error: 'Failed to fetch orders' });
+  }
+};
+
+export const getOneOrder = async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const order = await orderModel.findById(id); 
+    if (!order) {
+      return res.status(404).json({ error: 'Order not found' });
+    }
+    return res.status(200).json(order);
+  } catch (error) {
+    console.error('Error fetching order:', error);
+    return res.status(500).json({ error: 'Failed to fetch order' });
+  }
+};
--- my-app/BE/src/router/order.js
+++ my-app/BE/src/router/order.js
@@ -1,12 +1,17 @@
 import { Router } from 'express';
 import {
   createOrder,
+  getAllOrders,
+  getOneOrder
+
 } from '../controller/order.js';
 
 const order = Router();
 
 order
   .post('/create', createOrder)
+  .get("/get",getAllOrders)
+  .get("/get/:id",getOneOrder)
 
 
 export default order;
