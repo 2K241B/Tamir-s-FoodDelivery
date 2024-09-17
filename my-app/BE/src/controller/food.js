edf0930f4b15817abf3a363ed879097edef3a135
24LP4205 24LP4205 <24LP4205@G301-CC0207.local>
2024-09-17 14:51:01 +0800

    get one food

--- my-app/BE/src/controller/food.js
+++ my-app/BE/src/controller/food.js
@@ -28,3 +28,16 @@ export const getFoods = async (req, res) => {
       return res.status(500).json(error);
     }
   };
+
+
+  export const getFood = async (req, res) => {
+    const { id } = req.params;
+  
+    try {
+      const response = await foodModel.findById(id);
+      return res.status(200).json(response);
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json(error);
+    }
+  };
