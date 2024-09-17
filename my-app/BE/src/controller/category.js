16ae1e0178a0c402f36b3ccafef1016ec367e3a2
Tamirlp4205 <168177795+Tamirlp4205@users.noreply.github.com>
2024-09-17 12:38:27 +0800

    update category (#96)

--- my-app/BE/src/controller/category.js
+++ my-app/BE/src/controller/category.js
@@ -1,23 +1,40 @@
 import { categoryModel } from '../schema/category.js';
 
 export const createCategory = async (req, res) => {
-  const { name } = req.body;
+    const { name } = req.body;
 
-  try {
-    const response = await categoryModel.create({ name });
-    return res.status(200).json(response);
-  } catch (error) {
-    console.log(error);
-    return res.status(500).json(error);
-  }
+    try {
+        const response = await categoryModel.create({ name });
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
 };
 
 export const getCategory = async (req, res) => {
     try {
-      const response = await categoryModel.find();
-      return res.status(200).json(response);
+        const response = await categoryModel.find();
+        return res.status(200).json(response);
     } catch (error) {
-      console.log(error);
-      return res.status(500).json(error);
+        console.log(error);
+        return res.status(500).json(error);
     }
-  };
+};
+
+
+export const updateCategory = async (req, res) => {
+    const { id } = req.params;
+
+    const { name } = req.body;
+
+    try {
+        const response = await categoryModel.findByIdAndUpdate(id, {
+            name,
+        });
+        return res.status(200).json(response);
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json(error);
+    }
+};
