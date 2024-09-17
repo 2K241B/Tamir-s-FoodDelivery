190882040912aa734526c113ecc953af6895a2ab
Tamirlp4205 <168177795+Tamirlp4205@users.noreply.github.com>
2024-09-17 15:13:52 +0800

    card component (#118)
    Co-authored-by: 24LP4205 24LP4205 <24LP4205@G301-CC0207.local>

--- my-app/FE/src/app/page.js
+++ my-app/FE/src/app/page.js
@@ -1,11 +1,13 @@
 'use client';
 import HomeFea from '@/components/HomeFea';
+import Card from '@/components/card';
 
 export default function Home() {
 
   return (
     <main>
       <HomeFea />
+      <Card/>
     </main>
   );
 }
